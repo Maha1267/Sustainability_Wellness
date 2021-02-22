9d2f262e452ddf15fb710aac55600a2b19be9ce7
Maha1267 <64696983+Maha1267@users.noreply.github.com>
2021-02-22 22:12:24 +0530

    Add files via upload

---
+++ Chatbot.java
@@ -0,0 +1,16 @@
+
+package com.example.healthmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chatbot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot);
+
+    }
+}
